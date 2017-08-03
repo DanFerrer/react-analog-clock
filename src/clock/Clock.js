cb53408657d7b9694d2a4ae28d4827506113a3ac
DanFerrer <danielferrer212@gmail.com>
2017-08-03 16:47:57 -0400

    :lipstick: add ClockHand css

--- src/clock/Clock.js
+++ src/clock/Clock.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ClockFace from './ClockFace';
-// import ClockHand from './ClockHand';
+import ClockHand from './ClockHand';
 
 import './clock.scss';
 
@@ -17,6 +17,33 @@ class Clock extends React.Component {
 		return (
 			<svg id="clock" width={circleWidth} height={circleWidth} viewBox={`0 0 ${circleWidth} ${circleWidth}`}>
 				<ClockFace start={circleStart} radius={circleStart * 0.05}></ClockFace>
+				<ClockHand 
+					handId={'hour'}
+					handClass={'hour-arm'}
+					start={circleStart}
+					stop={circleStart - 2}
+					draw={circleStart / 2}
+					radius={circleStart * 0.85}
+				>
+				</ClockHand>
+				<ClockHand 
+					handId={'minute'}
+					handClass={'minute-arm'}
+					start={circleStart}
+					stop={circleStart - 2}
+					draw={circleStart * 0.25}
+					radius={circleStart * 0.85}
+				>
+				</ClockHand>
+				<ClockHand 
+					handId={'second'}
+					handClass={'second-arm'}
+					start={circleStart}
+					stop={circleStart + 50}
+					draw={circleStart * 0.20}
+					radius={circleStart * 0.85}
+				>
+				</ClockHand>
 			</svg>
 		);
 	}
