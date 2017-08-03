a9b80acb89047586a8e78cab66635f446542c551
DanFerrer <danielferrer212@gmail.com>
2017-08-03 18:10:21 -0400

    :tractor: finished ClockHand

--- src/clock/Clock.js
+++ src/clock/Clock.js
@@ -11,14 +11,62 @@ const circleStart = circleWidth / 2;
 class Clock extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			date: new Date()
+		};
+	}
+
+	componentDidMount() {
+		this.intervalId = setInterval(() => this.runClock(), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.intervalId);
+	}
+
+	getHandPlacements() {
+			const hours = this.state.date.getHours();
+			const minutes = this.state.date.getMinutes();
+			const seconds = this.state.date.getSeconds();
+
+			const hourPosition = (hours * 360 / 12) + (minutes * (360 / 60) / 12);
+			const minutePosition = (minutes * 360 / 60) + (minutes * (360 / 60) / 60);
+			const secondPosition = seconds * 360 /60;
+
+			const origin = `${circleStart}px ${circleStart}px`;
+
+			return {
+				hour: {
+					transformOrigin: origin,
+					transform: `rotate(${hourPosition}deg)`
+				},
+				minute: {
+					transformOrigin: origin,
+					transform: `rotate(${minutePosition}deg)`
+				},
+				second: {
+					transformOrigin: origin,
+					transform: `rotate(${secondPosition}deg)`
+				}
+			};
+	}
+
+	runClock() {
+		this.setState({
+			date: new Date()
+		});
 	}
 
 	render() {
+		const clockHandStyles = this.getHandPlacements();
+
 		return (
 			<svg id="clock" width={circleWidth} height={circleWidth} viewBox={`0 0 ${circleWidth} ${circleWidth}`}>
 				<ClockFace start={circleStart} radius={circleStart * 0.05}></ClockFace>
-				<ClockHand 
+				<ClockHand
 					handId='hour'
+					handStyles={clockHandStyles.hour}
 					handClass='hour-arm'
 					start={circleStart}
 					stop={circleStart - 2}
@@ -28,6 +76,7 @@ class Clock extends React.Component {
 				</ClockHand>
 				<ClockHand 
 					handId='minute'
+					handStyles={clockHandStyles.minute}
 					handClass='minute-arm'
 					start={circleStart}
 					stop={circleStart - 2}
@@ -35,8 +84,9 @@ class Clock extends React.Component {
 					radius={circleStart * 0.85}
 				>
 				</ClockHand>
-				<ClockHand 
+				<ClockHand
 					handId='second'
+					handStyles={clockHandStyles.second}
 					handClass='second-arm'
 					start={circleStart}
 					stop={circleStart + 50}
--- src/clock/ClockHand.js
+++ src/clock/ClockHand.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const ClockHand = (props) => {
 	return (
-		<g id={props.handId}>
+		<g id={props.handId} style={props.handStyles}>
 			<path className={props.handClass} d={`M${props.start} ${props.stop}V${props.draw}`}/>
 			<circle className='hidden' cx={props.start} cy={props.start} r={props.radius}/>
 		</g>
@@ -11,8 +11,9 @@ const ClockHand = (props) => {
 };
 
 ClockHand.propTypes = {
-	handId: PropTypes.number,
-	handClass: PropTypes.number,
+	handId: PropTypes.string,
+	handStyles: PropTypes.object,
+	handClass: PropTypes.string,
 	start: PropTypes.number,
 	stop: PropTypes.number,
 	draw: PropTypes.number,
